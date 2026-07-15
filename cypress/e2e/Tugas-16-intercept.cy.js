88ed28009bad540a671c80321a269bd9526cf504
alfaenhaydar <alfaenhaydar@gmail.com>
2026-07-16 02:49:31 +0700

    menambahkan file tugas 16 intercept cypress

--- cypress/e2e/Tugas-16-intercept.cy.js
+++ cypress/e2e/Tugas-16-intercept.cy.js
@@ -5,7 +5,12 @@ describe("Test Scenario Fungsi Login", () => {
     );
     cy.get('[name = "username"]').type("Admin");
     cy.get('[name = "password"]').type("admin123");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/action-summary",
+    ).as("getactionSummary");
     cy.get('[type="submit"]').click();
+    cy.wait("@getactionSummary").its("response.statusCode").should("eq", 200);
     cy.url().should("include", "dashboard/index");
   });
 
@@ -15,104 +20,104 @@ describe("Test Scenario Fungsi Login", () => {
     );
     cy.get('[name = "username"]').invoke("val", "Admin").trigger("input");
     cy.get('[name = "password"]').invoke("val", "admin123").trigger("input");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/shortcuts",
+    ).as("getshortcuts");
     cy.get('[type="submit"]').click();
+    cy.wait("@getshortcuts").its("response.statusCode").should("eq", 200);
     cy.url().should("include", "dashboard/index");
   });
 
-  it("TC03 - User login dengan username yang tidak diisi dan password valid", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-    );
-    cy.get('[name = "password"]').type("admin123");
-    cy.get('[type="submit"]').click();
-    cy.url().should("include", "login");
-  });
-
-  it("TC04 - User login dengan username valid dan password yang tidak diisi", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-    );
-    cy.get('[name = "username"]').type("Admin");
-    cy.get('[type="submit"]').click();
-    cy.url().should("include", "login");
-  });
-
-  it("TC05 - User login dengan username yang invalid dan password yang valid", () => {
+  it("TC03 - User login dengan username yang invalid dan password yang valid", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type("wakwaw");
     cy.get('[name = "password"]').type("admin123");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages",
+    ).as("getmessages");
     cy.get('[type="submit"]').click();
+    cy.wait("@getmessages").its("response.statusCode").should("eq", 304);
     cy.url().should("include", "login");
   });
 
-  it("TC06 - User login dengan username yang valid dan password yang invalid", () => {
+  it("TC04 - User login dengan username yang valid dan password yang invalid", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type("Admin");
     cy.get('[name = "password"]').type("aduhsalah");
+    cy.intercept(
+      "POST",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate",
+    ).as("postvalidate");
     cy.get('[type="submit"]').click();
+    cy.wait("@postvalidate").its("request.method").should("eq", "POST");
     cy.url().should("include", "login");
   });
 
-  it("TC07 - User login tanpa mengisi username dan password", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-    );
-    cy.get('[type="submit"]').click();
-    cy.url().should("include", "login");
-  });
-
-  it("TC08 - User login dengan kesalahan penulisan huruf besar atau kecil pada username dan menggunakan password yang valid", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-    );
-    cy.get('[name = "username"]').type("aDmin");
-    cy.get('[name = "password"]').type("admin123");
-    cy.get('[type="submit"]').click();
-    cy.url().should("include", "login");
-  });
-  // pada test case 8 TC08, terdapat bug karena user masih dapat login walaupun terdapat kesalahan penulisan huruf besar atau kecil pada username. Seharusnya user tidak dapat login jika terdapat kesalahan penulisan huruf besar atau kecil pada username.
-
-  it("TC09 - User login dengan username valid dan dengan kesalahan penulisan huruf besar atau kecil pada password", () => {
+  it("TC05 - User login dengan username valid dan dengan kesalahan penulisan huruf besar atau kecil pada password", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type("admin");
     cy.get('[name = "password"]').type("Admin123");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages",
+    ).as("getmessages");
     cy.get('[type="submit"]').click();
+    cy.wait("@getmessages")
+      .its("request.url")
+      .should("include", "/index.php/core/i18n/messages");
     cy.url().should("include", "login");
   });
 
-  it("TC10 - User login menggunakan username dengan emoticon dan password yang valid", () => {
+  it("TC06 - User login menggunakan username dengan emoticon dan password yang valid", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type("admin😃");
     cy.get('[name = "password"]').type("Admin123");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    ).as("loginpage");
     cy.get('[type="submit"]').click();
+    cy.wait("@loginpage").its("response.statusCode").should("eq", 200);
     cy.url().should("include", "login");
   });
 
-  it("TC11 - User login dengan username yang valid dan memasukan password dengan emoticon", () => {
+  it("TC07 - User login dengan username yang valid dan memasukan password dengan emoticon", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type("admin");
     cy.get('[name = "password"]').type("Admin123😃");
+    cy.intercept(
+      "GET",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    ).as("loginpage");
     cy.get('[type="submit"]').click();
+    cy.wait("@loginpage").its("request.method").should("eq", "GET");
     cy.url().should("include", "login");
   });
 
-  it("TC12 - User login dengan awal username diawali dengan spasi", () => {
+  it("TC08 - User login dengan awal username diawali dengan spasi", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
     cy.get('[name = "username"]').type(" admin");
     cy.get('[name = "password"]').type("Admin123😃");
+    cy.intercept(
+      "POST",
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate",
+    ).as("postvalidate");
     cy.get('[type="submit"]').click();
+    cy.wait("@postvalidate").its("response.statusCode").should("eq", 302);
     cy.url().should("include", "login");
   });
 });
